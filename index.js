107449ea85d43b380eab8806403b0b3bce4f7d21
Marcelo Reyna <lepozepo@gmail.com>
2016-06-18 11:19:37 -0700

    Explicitly wrap components

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var React = require('react');
 var ReactRouter = require('react-router');
-var forIn = require('lodash/forIn');
 
-var wrappedReactRouter = {};
-forIn(ReactRouter, function(component, k) {
-	wrappedReactRouter[k] = React.createFactory(component);
+var wrappedReactRouter = Object.assign({}, ReactRouter, {
+	Router: React.createFactory(ReactRouter.Router),
+	Link: React.createFactory(ReactRouter.Link),
+	IndexLink: React.createFactory(ReactRouter.IndexLink),
+	RouterContext: React.createFactory(ReactRouter.RouterContext),
+	Route: React.createFactory(ReactRouter.Route),
+	Redirect: React.createFactory(ReactRouter.Redirect),
+	IndexRoute: React.createFactory(ReactRouter.IndexRoute),
+	IndexRedirect: React.createFactory(ReactRouter.IndexRedirect),
 });
 
 module.exports = wrappedReactRouter;
